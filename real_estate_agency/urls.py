1f8005c43d7ccbb544263acde8d99fe10257686f
Ilya Osipov <070809010@mail.ru>
2019-08-06 15:55:42 +0300

    Fix urls, first was triggered all the time

--- real_estate_agency/urls.py
+++ real_estate_agency/urls.py
@@ -4,7 +4,7 @@ from django.contrib import admin
 from property import views
 
 urlpatterns = [
-    url(r'', views.show_flats),
-    url(r'search/', views.show_flats),
+    url(r'^$', views.show_flats),
+    url(r'^search/$', views.show_flats),
     url(r'^admin/', admin.site.urls),
 ]
