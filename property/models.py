e9d7bd8729875a837e2699cb17122ddb5976dc62
Evgeny Evseev <pelid80@gmail.com>
2019-08-29 22:43:27 +0300

    fix null/blank attributes for Flat model

--- property/models.py
+++ property/models.py
@@ -16,11 +16,11 @@ class Flat(models.Model):
     floor = models.CharField("Этаж", max_length=3, help_text='Первый этаж, последний этаж, пятый этаж')
 
     rooms_number = models.IntegerField("Количество комнат в квартире", db_index=True)
-    living_area = models.IntegerField("количество жилых кв.метров", null=True, db_index=True)
+    living_area = models.IntegerField("количество жилых кв.метров", null=True, blank=True, db_index=True)
 
-    has_balcony = models.NullBooleanField("Наличие балкона", null=True, db_index=True)
+    has_balcony = models.NullBooleanField("Наличие балкона", db_index=True)
     active = models.BooleanField("Активно-ли объявление", db_index=True)
-    construction_year = models.IntegerField("Год постройки здания", null=True, db_index=True)
+    construction_year = models.IntegerField("Год постройки здания", null=True, blank=True, db_index=True)
 
     def __str__(self):
         return f"{self.town}, {self.address} ({self.price}р.)"
